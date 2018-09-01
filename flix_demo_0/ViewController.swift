634fb762350c2464731956b943ede37f9e9163bb
Fahit Ahmed <fahit.ahmed@baruchmail.cuny.edu>
2018-08-31 20:39:42 -0400

    Initial Commit

---
+++ flix_demo_0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flix_demo_0
+//
+//  Created by Fahit Ahmed on 8/31/18.
+//  Copyright © 2018 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
